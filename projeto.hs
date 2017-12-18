260de5051c14ae79ae38e62c2f7a2df89e2e658c
Rafael <rafael.maciel@ccc.ufcg.edu.br>
2017-12-18 13:10:55 -0300

    Iniciando projeto

--- projeto.hs
+++ projeto.hs
@@ -2,4 +2,18 @@ import System.Random
 
 main :: IO ()
 main = do
- print "Projeto APLP Haskell"
+ mostrarMenu
+
+ opcao <- getLine
+ 
+ print "FIM"
+
+mostrarMenu :: IO ()
+mostrarMenu = do
+ putStrLn ("------------------------")
+ putStrLn ("Projeto APLP - Haskell")
+ putStrLn ("------------------------")
+ putStrLn ("Escolha uma modo de jogo:")
+ putStrLn ("1 - Um jogador")
+ putStrLn ("2 - Dois jogadores")
+ putStrLn ("3 - Sair")
