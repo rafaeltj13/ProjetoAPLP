4318edce834063b9a6c4d8517334e4f49cd0b728
Rafael <rafael.maciel@ccc.ufcg.edu.br>
2017-12-21 19:24:13 -0300

    Finalizando projeto

--- projeto.hs
+++ projeto.hs
@@ -1,5 +1,6 @@
 import System.Random as Random
 
+--Main da aplicação
 main :: IO ()
 main = do
  mostrarMenu
@@ -32,99 +33,7 @@ umJogador = do
 
   rodadas1jogador rodadaInicial placar1 placar2
  
---Modo de jogo para dois jogadores
-doisJogadores :: IO ()
-doisJogadores = do
-
- let rodadaInicial = 1
- let placar1 = 0
- let placar2 = 0
-
- rodadas2jogadores rodadaInicial placar1 placar2
-
---Mostra as opções de chute
-opcoesDeChute :: IO ()
-opcoesDeChute = do
- putStrLn ("Selecione um local de chute:")
- putStrLn ("1 - Esquerda Baixo")
- putStrLn ("2 - Esquerda Cima")
- putStrLn ("3 - Meio Baixo")
- putStrLn ("4 - Meio Cima")
- putStrLn ("5 - Direita Baixo")
- putStrLn ("6 - Direita Cima")
-
---Mostra o placar da partida de um jogador
-mostrarPlacar :: Int -> Int -> IO ()
-mostrarPlacar placar1 placar2 = do
-  putStrLn(" ")
-  putStrLn("Jogador: "++ (show placar1) ++ " x " ++ (show placar2) ++ " :PC")
-  putStrLn(" ")
-
---Mostra o placar da partida entre dois jogadores
-mostrarPlacar2 :: Int -> Int -> IO ()
-mostrarPlacar2 placar1 placar2 = do
-  putStrLn(" ")
-  putStrLn("Jogador 1: "++ (show placar1) ++ " x " ++ (show placar2) ++ " :Jogador 2")
-  putStrLn(" ")
-
---Checa se o chute foi gol
-checarGol :: Int -> IO ()
-checarGol foiGol = do
- if(foiGol == 1) then putStrLn("GOOOOOOOL!!!")
-  else putStrLn("ERROOOOU!!!")
-
-chutar :: Int -> Int -> Int
-chutar localDoChute puloGoleiro=
-  if(localDoChute == puloGoleiro) then 0
-   else 1
-
---Calcula o vencedor da partida
-resultado :: Int -> Int -> String
-resultado placar1 placar2 =
- if(placar1 > placar2 ) then "Jogador 1 venceu!!!"
-  else
-   if(placar1 < placar2) then"Jogador 2 venceu!!!"
-    else "Empate!!!"
-
-resultadoUmJogador :: Int -> Int -> String
-resultadoUmJogador placar1 placar2 =
-  if(placar1 > placar2 ) then "Jogador venceu!!!"
-   else
-    if(placar1 < placar2) then"PC venceu!!!"
-     else "Empate!!!"
-
-rodadas2jogadores :: Int -> Int -> Int -> IO()
-rodadas2jogadores 6 placar1 placar2 = do
-  mostrarPlacar2 placar1 placar2
-  let resultadoFinal = resultado placar1 placar2
-  putStrLn(resultadoFinal)
-
-rodadas2jogadores rodadaAtual placar1 placar2 = do
-    putStrLn(" ")
-    imprimirRodada rodadaAtual
-    mostrarPlacar2 placar1 placar2
-    putStrLn("JOGADOR 1:")
-    opcoesDeChute
-    localChute1 <- getLine
-    let chute1 = read localChute1 :: Int
-    puloGoleiro <- randomRIO(chute1, chute1+1)
-    let foiGol = chutar chute1 puloGoleiro
-    checarGol(foiGol)
-
-    let novoPlacar1 = placar1 + foiGol
-
-    putStrLn("JOGADOR 2:")
-    opcoesDeChute
-    localChute2 <- getLine
-    let chute2 = read localChute2 :: Int
-    puloGoleiro <- randomRIO(chute2, chute2+1)
-    let foiGol = chutar chute2 puloGoleiro
-    checarGol(foiGol)
-
-    let novoPlacar2 = placar2 + foiGol
-
-    rodadas2jogadores (rodadaAtual+1) novoPlacar1 novoPlacar2
-
+--Executa as cinco rodadas para o modo de um jogador
 rodadas1jogador :: Int -> Int -> Int -> IO()
 rodadas1jogador 6 placar1 placar2 = do
   mostrarPlacar placar1 placar2
@@ -142,6 +51,7 @@ rodadas1jogador rodadaAtual placar1 placar2 = do
   let chute1 = read localChute1 :: Int
   puloGoleiro <- randomRIO(chute1, chute1+1)
   let foiGol = chutar chute1 puloGoleiro
+  imprimirPuloGoleiro chute1 foiGol
   checarGol(foiGol)
   let novoPlacar1 = placar1 + foiGol
 
@@ -149,11 +59,88 @@ rodadas1jogador rodadaAtual placar1 placar2 = do
   chute2 <- randomRIO(1 :: Int, 6 :: Int)
   puloGoleiro <- randomRIO(chute2, chute2+1)
   let foiGol = chutar chute2 puloGoleiro
+  imprimirPuloGoleiro chute2 foiGol
   checarGol(foiGol)
   let novoPlacar2 = placar2 + foiGol
 
   rodadas1jogador (rodadaAtual+1) novoPlacar1 novoPlacar2
 
+--Mostra o placar da partida de um jogador
+mostrarPlacar :: Int -> Int -> IO ()
+mostrarPlacar placar1 placar2 = do
+  putStrLn(" ")
+  putStrLn("Jogador: "++ (show placar1) ++ " x " ++ (show placar2) ++ " :PC")
+  putStrLn(" ")
+
+--Mostra o resultado da partida no modo de um jogador
+resultadoUmJogador :: Int -> Int -> String
+resultadoUmJogador placar1 placar2 =
+  if(placar1 > placar2 ) then "Jogador venceu!!!"
+   else
+    if(placar1 < placar2) then"PC venceu!!!"
+     else "Empate!!!"
+
+--Modo de jogo para dois jogadores
+doisJogadores :: IO ()
+doisJogadores = do
+
+ let rodadaInicial = 1
+ let placar1 = 0
+ let placar2 = 0
+
+ rodadas2jogadores rodadaInicial placar1 placar2
+
+--Executa as cinco rodadas para o modo de dois jogadores
+rodadas2jogadores :: Int -> Int -> Int -> IO()
+rodadas2jogadores 6 placar1 placar2 = do
+  mostrarPlacar2 placar1 placar2
+  let resultadoFinal = resultado placar1 placar2
+  putStrLn(resultadoFinal)
+
+rodadas2jogadores rodadaAtual placar1 placar2 = do
+  putStrLn(" ")
+  imprimirRodada rodadaAtual
+  mostrarPlacar2 placar1 placar2
+  putStrLn("JOGADOR 1:")
+  opcoesDeChute
+  localChute1 <- getLine
+  let chute1 = read localChute1 :: Int
+  puloGoleiro <- randomRIO(chute1, chute1+1)
+  let foiGol = chutar chute1 puloGoleiro
+  imprimirPuloGoleiro chute1 foiGol
+  checarGol(foiGol)
+
+  let novoPlacar1 = placar1 + foiGol
+
+  putStrLn("JOGADOR 2:")
+  opcoesDeChute
+  localChute2 <- getLine
+  let chute2 = read localChute2 :: Int
+  puloGoleiro <- randomRIO(chute2, chute2+1)
+  let foiGol = chutar chute2 puloGoleiro
+  imprimirPuloGoleiro chute2 foiGol
+  checarGol(foiGol)
+
+  let novoPlacar2 = placar2 + foiGol
+
+  rodadas2jogadores (rodadaAtual+1) novoPlacar1 novoPlacar2
+ 
+--Mostra o placar da partida entre dois jogadores
+mostrarPlacar2 :: Int -> Int -> IO ()
+mostrarPlacar2 placar1 placar2 = do
+  putStrLn(" ")
+  putStrLn("Jogador 1: "++ (show placar1) ++ " x " ++ (show placar2) ++ " :Jogador 2")
+  putStrLn(" ")
+
+--Calcula o vencedor da partida no modo de dois jogadores
+resultado :: Int -> Int -> String
+resultado placar1 placar2 =
+ if(placar1 > placar2 ) then "Jogador 1 venceu!!!"
+  else
+   if(placar1 < placar2) then"Jogador 2 venceu!!!"
+    else "Empate!!!"
+
+--Mostra a rodada atual
 imprimirRodada :: Int -> IO()
 imprimirRodada rodadaAtual
  | rodadaAtual == 1 = putStrLn("Primeira Rodada!")
@@ -162,3 +149,138 @@ imprimirRodada rodadaAtual
  | rodadaAtual == 4 = putStrLn("Quarta Rodada!")
  | rodadaAtual == 5 = putStrLn("Quinta Rodada!")
  | otherwise = putStrLn("Fim de jogo")
+
+--Checa se o chute foi gol
+chutar :: Int -> Int -> Int
+chutar localDoChute puloGoleiro=
+  if(localDoChute == puloGoleiro) then 0
+   else 1
+
+--Checa se o chute foi gol
+checarGol :: Int -> IO ()
+checarGol foiGol = do
+ if(foiGol == 1) then putStrLn("GOOOOOOOL!!!")
+  else putStrLn("ERROOOOU!!!")
+
+--Mostra as opções de chute
+opcoesDeChute :: IO ()
+opcoesDeChute = do
+ putStrLn ("Selecione um local de chute:")
+ putStrLn ("1 - Esquerda Baixo")
+ putStrLn ("2 - Esquerda Cima")
+ putStrLn ("3 - Meio Baixo")
+ putStrLn ("4 - Meio Cima")
+ putStrLn ("5 - Direita Baixo")
+ putStrLn ("6 - Direita Cima")
+
+--Impressões para o pulo do goleiro de acordo com a posição
+imprimirPuloGoleiro :: Int -> Int -> IO()
+imprimirPuloGoleiro localChute foiGol
+ | (localChute == 1 && foiGol == 1) = golBaixoEsquerda
+ | (localChute == 1 && foiGol == 0) = errouBaixoEsquerda
+ | (localChute == 2 && foiGol == 1) = golCimaEsquerda 
+ | (localChute == 2 && foiGol == 0) = errouCimaEsquerda
+ | (localChute == 3 && foiGol == 1) = golBaixoMeio
+ | (localChute == 3 && foiGol == 0) = errouBaixoMeio
+ | (localChute == 4 && foiGol == 1) = golCimaMeio
+ | (localChute == 4 && foiGol == 0) = errouCimaMeio
+ | (localChute == 5 && foiGol == 1) = golBaixoDireita
+ | (localChute == 5 && foiGol == 0) = errouBaixoDireita
+ | (localChute == 6 && foiGol == 1) = golCimaDireita
+ | otherwise = errouCimaDireita
+
+golBaixoEsquerda :: IO()
+golBaixoEsquerda = do
+ putStrLn("---------------------------")
+ putStrLn("|   |  O  |               |")
+ putStrLn("|    |_|_|                |")
+ putStrLn("|      |                  |")
+ putStrLn("|  O  | |                 |")
+
+errouBaixoEsquerda :: IO()
+errouBaixoEsquerda = do
+ putStrLn("---------------------------")
+ putStrLn("| |  O  |                 |")
+ putStrLn("|  |_|_|                  |")
+ putStrLn("|    |                    |")
+ putStrLn("|   X |                   |")
+
+golCimaEsquerda :: IO()
+golCimaEsquerda = do
+ putStrLn("---------------------------")
+ putStrLn("| O |  O  |               |")
+ putStrLn("|    |_|_|                |")
+ putStrLn("|      |                  |")
+ putStrLn("|     | |                 |")
+
+errouCimaEsquerda :: IO()
+errouCimaEsquerda = do
+ putStrLn("---------------------------")
+ putStrLn("|  X O  |                 |")
+ putStrLn("|  |_|_|                  |")
+ putStrLn("|    |                    |")
+ putStrLn("|   | |                   |")
+
+golBaixoMeio :: IO()
+golBaixoMeio = do
+ putStrLn("---------------------------")
+ putStrLn("|        |  O  |          |")
+ putStrLn("|         |_|_|           |")
+ putStrLn("|           |             |")
+ putStrLn("|          |O|            |")
+
+errouBaixoMeio :: IO()
+errouBaixoMeio = do
+ putStrLn("---------------------------")
+ putStrLn("|        |  O  |          |")
+ putStrLn("|         |_|_|           |")
+ putStrLn("|           |             |")
+ putStrLn("|          X |            |")
+
+golCimaMeio :: IO()
+golCimaMeio = do
+ putStrLn("---------------------------")
+ putStrLn("|        O  |  O  |       |")
+ putStrLn("|            |_|_|        |")
+ putStrLn("|              |          |")
+ putStrLn("|             | |         |")
+
+errouCimaMeio :: IO()
+errouCimaMeio = do
+ putStrLn("---------------------------")
+ putStrLn("|        |  x  |          |")
+ putStrLn("|         |_|_|           |")
+ putStrLn("|           |             |")
+ putStrLn("|          | |            |")
+
+golBaixoDireita :: IO()
+golBaixoDireita = do
+ putStrLn("---------------------------")
+ putStrLn("|              |  O  |    |")
+ putStrLn("|               |_|_|     |")
+ putStrLn("|                 |       |")
+ putStrLn("|                | |   O  |")
+
+errouBaixoDireita :: IO()
+errouBaixoDireita = do
+ putStrLn("---------------------------")
+ putStrLn("|                |  O  |  |")
+ putStrLn("|                 |_|_|   |")
+ putStrLn("|                   |     |")
+ putStrLn("|                  | X    |")
+
+golCimaDireita :: IO()
+golCimaDireita = do
+ putStrLn("---------------------------")
+ putStrLn("|              |  O  |  O |")
+ putStrLn("|               |_|_|     |")
+ putStrLn("|                 |       |")
+ putStrLn("|                | |      |")
+
+errouCimaDireita :: IO()
+errouCimaDireita = do
+ putStrLn("---------------------------")
+ putStrLn("|                |  O  X  |")
+ putStrLn("|                 |_|_|   |")
+ putStrLn("|                   |     |")
+ putStrLn("|                  | |    |")
